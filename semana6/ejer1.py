742a244bd637dac3f3fea54d10a4c6f3f5b9544b
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2025-10-04 21:00:35 -0500

    Update ejer1.py

--- semana6/ejer1.py
+++ semana6/ejer1.py
@@ -8,7 +8,7 @@ class Animal:
         self.peso = peso           # en kilogramos
         self.habitat = habitat     # ejemplo: "selva", "sabana", etc.
 
-    # Método especial: representación legible (para usuarios)
+    # Método especial: devuelve  una cadena de texto
     def __str__(self):
         return f"""Información del Animal
 Nombre   : {self.nombre}
@@ -19,7 +19,7 @@ Peso     : {self.peso} kg
 Hábitat  : {self.habitat}
 """
 
-    # Método especial: representación técnica (para desarrolladores)
+    # Método especial: devolver una cadena de texto que muestra cómo se creó el objeto.
     def __repr__(self):
         return (
             f"Animal('{self.nombre}', '{self.color}', {self.edad}, "
@@ -34,20 +34,10 @@ Hábitat  : {self.habitat}
     def comer(self, alimento):
         return f"{self.nombre} está comiendo {alimento}."
 
-    # Método especial: comparación por edad (==)
-    def __eq__(self, otro):
-        if isinstance(otro, Animal):
-            return self.edad == otro.edad
-        return False
-
-    # Método especial: comparación por peso (<)
-    def __lt__(self, otro):
-        if isinstance(otro, Animal):
-            return self.peso < otro.peso
-        return False
+    
 
 
-#  Ejemplo de uso
+# objetos
 animal1 = Animal("Lobo", "Gris", 5, "Canino", 35.5, "Bosque")
 animal2 = Animal("Tigre", "Naranja", 5, "Felino", 220.7, "Selva")
 
@@ -56,4 +46,5 @@ print(animal1)
 print(animal1.dormir())
 print(animal1.comer("carne"))
 print("informacion", repr(animal1))
+
 
